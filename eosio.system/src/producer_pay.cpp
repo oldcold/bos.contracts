69eedc764cf03c9858972c275edba6466588ae40
BOS-boscore <bangkokeos66@gmail.com>
2019-01-02 14:45:11 +0800

    update block producers once every minute

--- eosio.system/src/producer_pay.cpp
+++ eosio.system/src/producer_pay.cpp
@@ -52,8 +52,8 @@ namespace eosiosystem {
          });
       }
 
-      /// only update block producers once every minute, block_timestamp is in half seconds
-      if( timestamp.slot - _gstate.last_producer_schedule_update.slot > 120 ) {
+      /// only update block producers once every minute, block_timestamp is in one seconds
+      if( timestamp.slot - _gstate.last_producer_schedule_update.slot > 60 ) {
          update_elected_producers( timestamp );
 
          if( (timestamp.slot - _gstate.last_name_close.slot) > blocks_per_day ) {
