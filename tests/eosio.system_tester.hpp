34b7a5764c159da50aa2a0688444a36a22a6ec15
vlbos <lisheng2007@hotmail.com>
2019-03-12 11:35:43 +0800

    update system test code

--- tests/eosio.system_tester.hpp
+++ tests/eosio.system_tester.hpp
@@ -84,6 +84,12 @@ public:
       create_account_with_resources( N(bob111111111), config::system_account_name, core_sym::from_string("0.4500"), false );
       create_account_with_resources( N(carol1111111), config::system_account_name, core_sym::from_string("1.0000"), false );
 
+
+      create_account_with_resources( N(actorblklst1), config::system_account_name, core_sym::from_string("1.0000"), false );
+      create_account_with_resources( N(contrblklst1), config::system_account_name, core_sym::from_string("0.4500"), false );
+      create_account_with_resources( N(resgreylst12), config::system_account_name, core_sym::from_string("1.0000"), false );
+
+
       BOOST_REQUIRE_EQUAL( core_sym::from_string("1000000000.0000"), get_balance("eosio")  + get_balance("eosio.ramfee") + get_balance("eosio.stake") + get_balance("eosio.ram") );
    }
 
